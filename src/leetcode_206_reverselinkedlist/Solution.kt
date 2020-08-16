7222ca417d9e24bc67c21be09a4678c926023b8b
Khoi Nguyen <akhoi90@gmail.com>
2020-08-16 22:07:25 +0700

    Init commit

---
+++ src/leetcode_206_reverselinkedlist/Solution.kt
@@ -0,0 +1,26 @@
+package leetcode_206_reverselinkedlist
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class ListNode(var value: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    fun reverseList(head: ListNode?): ListNode? {
+        println("test")
+        return null
+    }
+}
+
+fun main(args: Array<String>) {
+    Solution().reverseList(null)
+}

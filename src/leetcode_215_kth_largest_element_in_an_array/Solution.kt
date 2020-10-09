caa2a1d650376fef68075155a660d2c41f41fbf6
Khoi Nguyen <akhoi90@gmail.com>
2020-10-09 11:35:40 +0700

    leetcode_215_kth_largest_element_in_an_array

---
+++ src/leetcode_215_kth_largest_element_in_an_array/Solution.kt
@@ -0,0 +1,16 @@
+package leetcode_215_kth_largest_element_in_an_array
+
+import java.util.*
+
+class Solution {
+    fun findKthLargest(nums: IntArray, k: Int): Int {
+        val queue = PriorityQueue<Int>(k + 1)// { i1, i2 -> i2 - i1 }
+        nums.forEach { item ->
+            queue.add(item)
+            if (queue.size > k)
+                queue.poll()
+        }
+
+        return queue.peek()
+    }
+}

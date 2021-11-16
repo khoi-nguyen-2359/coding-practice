e38d154dc390b52669b21bc8330c6a3773686dd2
Khoi Nguyen <akhoi90@gmail.com>
2021-11-16 21:52:51 +0700

    leetcode_14_longest_common_prefix

---
+++ src/leetcode_14_longest_common_prefix/Solution.kt
@@ -0,0 +1,22 @@
+package leetcode_14_longest_common_prefix
+
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        val result = StringBuilder()
+        var curr: Int
+        var i = 0
+        while (strs.isNotEmpty()) {
+            curr = strs[0].getOrNull(i)?.toInt()
+                    ?: return result.toString()
+            for (k in 1 until strs.size) {
+                val si = strs[k].getOrNull(i)
+                if (si == null || si.toInt() != curr) {
+                    return result.toString()
+                }
+            }
+            result.append(curr.toChar())
+            ++i
+        }
+        return result.toString()
+    }
+}

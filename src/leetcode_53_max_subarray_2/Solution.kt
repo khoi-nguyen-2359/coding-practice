e18ce8e6fb25e4fbb7739900dc9eb6a2b9cd68f5
Khoi Nguyen <akhoi90@gmail.com>
2022-02-27 09:41:58 +0700

    leetcode_53_max_subarray_2

---
+++ src/leetcode_53_max_subarray_2/Solution.kt
@@ -0,0 +1,20 @@
+package leetcode_53_max_subarray_2
+
+class Solution {
+    fun maxSubArray(nums: IntArray): Int {
+        var max = nums[0]
+        var currSum = 0
+        for (i in nums.indices) {
+            currSum += nums[i]
+            if (nums[i] > currSum) {
+                currSum = nums[i]
+            }
+            if (currSum > max) {
+                max = currSum
+            }
+        }
+        return max
+    }
+}
+
+// [-2,1,-3,4,-1,2,1,-5,4]

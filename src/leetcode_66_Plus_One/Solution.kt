771b69d3527cd09e2df6deb4e8725595b3878e60
Khoi Nguyen <akhoi90@gmail.com>
2022-02-27 15:49:25 +0700

    leetcode_66_Plus_One

---
+++ src/leetcode_66_Plus_One/Solution.kt
@@ -0,0 +1,24 @@
+package leetcode_66_Plus_One
+
+class Solution {
+    fun plusOne(digits: IntArray): IntArray {
+        val answer = IntArray(digits.size + 1)
+        var carrier = 1
+        for (i in digits.size - 1 downTo 0) {
+            var sum = digits[i] + carrier
+            carrier = sum / 10
+            sum %= 10
+            answer[i + 1] = sum
+        }
+        return if (carrier != 0) {
+            answer[0] = 1
+            answer
+        } else {
+            IntArray(digits.size) { index ->
+                answer[index + 1]
+            }
+        }
+    }
+}
+
+// Input: digits = [4,3,2,9]

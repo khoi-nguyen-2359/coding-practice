46503e9a2416a22100c9b31fc940085e503fd5ad
Khoi Nguyen <akhoi90@gmail.com>
2022-03-16 18:15:55 +0700

    misc_adding_one_to_linked_list_num

---
+++ src/misc_adding_one_to_linked_list_num/SolutionRecursive.kt
@@ -0,0 +1,96 @@
+package misc_adding_one_to_linked_list_num
+
+class Node(var value: Int) {
+    var next: Node? = null
+}
+
+class SolutionRecursive {
+    private var carry = 1
+    fun addOne(head: Node): Node {
+        carry = 1
+        addRecursively(head)
+        return if (carry != 0) {
+            Node(carry).apply {
+                next = head
+            }
+        } else {
+            head
+        }
+    }
+
+    private fun addRecursively(node: Node?) {
+        if (node == null) {
+            return
+        }
+
+        addRecursively(node.next)
+
+        val s = node.value + carry
+        carry = s / 10
+        node.value = s % 10
+    }
+}
+
+class SolutionInPlace {
+    fun addOne(head: Node): Node {
+        val reversed = reverse(head)
+        var curr: Node? = reversed
+        var carry = 1
+        var prev: Node? = curr
+        while (curr != null) {
+            val s = curr.value + carry
+            curr.value = s % 10
+            carry = s / 10
+
+            prev = curr
+            curr = curr.next
+        }
+        if (carry != 0) {
+            prev?.next = Node(carry)
+        }
+
+        return reverse(reversed)
+    }
+
+    private fun reverse(head: Node): Node {
+        var curr: Node? = head
+        var next: Node? = null
+        while (curr != null) {
+            val tmp = curr.next
+            curr.next = next
+            next = curr
+            curr = tmp
+
+            if (curr == null) {
+                return next
+            }
+        }
+
+        return Node(-1)
+    }
+}
+
+fun main() {
+    val head = Node(9).apply {
+        next = Node(9).apply {
+            next = Node(9).apply {
+                next = Node(9).apply {
+                    next = Node(9).apply {
+                        next = Node(9)
+                    }
+                }
+            }
+        }
+    }
+
+    SolutionInPlace().addOne(head).let {
+        var c: Node? = it
+        while (c != null) {
+            println(c.value)
+            c = c.next
+        }
+    }
+}
+/*
+[2]->[4]->[5]->[7]->[3]->[6]
+ */

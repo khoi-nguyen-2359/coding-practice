aec76123c74716f4011ac0ff77845eab56fe4e5c
Khoi Nguyen <akhoi90@gmail.com>
2022-05-11 22:36:40 +0700

    leetcode_23_Merge_k_Sorted_Lists

--- src/ListNode.kt
+++ src/ListNode.kt
@@ -8,4 +8,20 @@ class ListNode(var `val`: Int) {
             curr = curr.next
         }
     }
+
+    companion object {
+        fun create(vararg values: Int): ListNode? {
+            if (values.isEmpty()) {
+                return null
+            }
+            val result = ListNode(values[0])
+            var c: ListNode? = result
+            for (i in 1 until values.size) {
+                c?.next = ListNode(values[i])
+                c = c?.next
+            }
+
+            return result
+        }
+    }
 }

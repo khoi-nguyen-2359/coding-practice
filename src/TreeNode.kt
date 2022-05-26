02a67c1c5ff7e34d027f30fe499f282d2653954a
Khoi Nguyen <akhoi90@gmail.com>
2022-05-26 23:03:14 +0700

    leetcode_124_Binary_Tree_Maximum_Path_Sum

--- src/TreeNode.kt
+++ src/TreeNode.kt
@@ -1,4 +1,40 @@
+import com.sun.source.tree.Tree
+import java.util.Queue
+import java.util.concurrent.ArrayBlockingQueue
+
 class TreeNode(var `val`: Int) {
     var left: TreeNode? = null
     var right: TreeNode? = null
+
+    companion object {
+        fun create(vararg values: Int?): TreeNode? {
+            if (values.isEmpty()) {
+                return null
+            }
+
+            val nodeArray = Array<TreeNode?>(values.size) { null }
+            for (i in values.indices) {
+                val nodeValue = values[i]
+                if (nodeArray[i] == null && nodeValue != null) {
+                    nodeArray[i] = TreeNode(nodeValue)
+                }
+                val leftValue = values.getOrNull(2*i + 1)
+                val rightValue = values.getOrNull(2*i + 2)
+                var leftNode: TreeNode? = null
+                if (leftValue != null) {
+                    leftNode = TreeNode(leftValue)
+                    nodeArray[2*i+1] = leftNode
+                }
+
+                var rightNode: TreeNode? = null
+                if (rightValue != null) {
+                    rightNode = TreeNode(rightValue)
+                    nodeArray[2*i+2] = rightNode
+                }
+                nodeArray[i]?.left = leftNode
+                nodeArray[i]?.right = rightNode
+            }
+            return nodeArray[0]
+        }
+    }
 }
---
+++ src/leetcode_124_Binary_Tree_Maximum_Path_Sum/Solution.kt
@@ -0,0 +1,49 @@
+package leetcode_124_Binary_Tree_Maximum_Path_Sum
+
+import TreeNode
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    private var result = Int.MIN_VALUE
+    fun maxPathSum(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+        result = Int.MIN_VALUE
+        max_gain(root)
+        return result
+    }
+
+    private fun max_gain(node: TreeNode?): Int {
+        if (node == null) {
+            return 0
+        }
+        val leftMaxGain = max_gain(node.left)
+        val rightMaxGain = max_gain(node.right)
+        val path = leftMaxGain + rightMaxGain + node.`val`
+        if (path > result) {
+            result = path
+        }
+
+        return Math.max(Math.max(leftMaxGain, rightMaxGain) + node.`val`, 0)
+    }
+}
+
+fun main() {
+    arrayOf(
+            TreeNode.create(1,2,3),
+            TreeNode.create(-10,9,20,null,null,15,7),
+            TreeNode.create(2,-1),
+    ).forEach {
+        Solution().maxPathSum(it).let(::println)
+    }
+}

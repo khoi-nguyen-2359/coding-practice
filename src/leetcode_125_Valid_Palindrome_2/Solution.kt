2ebcd06faad6a0472e1ee158e933e7acf5977534
Khoi Nguyen <akhoi90@gmail.com>
2022-06-08 20:09:31 +0700

    leetcode_125_Valid_Palindrome_2

---
+++ src/leetcode_125_Valid_Palindrome_2/Solution.kt
@@ -0,0 +1,45 @@
+package leetcode_125_Valid_Palindrome_2
+
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        var l = 0
+        var r = s.length - 1
+        while (l < r) {
+            while (l < s.length && !s[l].isLetterOrDigit()) {
+                ++l
+            }
+            while (r >= 0 && !s[r].isLetterOrDigit()) {
+                --r
+            }
+            if (l >= r) {
+                break
+            }
+            if (s[l].toLowerCase() != s[r].toLowerCase()) {
+                return false
+            }
+            ++l
+            --r
+        }
+
+        return true
+    }
+}
+
+fun main() {
+    arrayOf(
+            "A man, a plan, a canal: Panama",
+            "race a car",
+            " ",
+            ".,",
+            "",
+            "a.",
+    ).forEach {
+        Solution().isPalindrome(it).let(::println)
+    }
+}
+
+/*
+Input: s = "A man, a plan, a canal: Panama"
+Input: s = " man  "
+l = 0, r = 5 -> l = 1, r = 3 -> 0 ? 2
+ */

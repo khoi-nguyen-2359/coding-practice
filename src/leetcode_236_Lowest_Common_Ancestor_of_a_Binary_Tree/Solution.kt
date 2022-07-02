541552bd27d0c2319ab082060bca309770817157
Khoi Nguyen <akhoi90@gmail.com>
2022-07-03 00:12:24 +0700

    leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree

--- src/leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.kt
+++ src/leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.kt
@@ -1,7 +1,9 @@
 package leetcode_236_Lowest_Common_Ancestor_of_a_Binary_Tree
 
 import TreeNode
+import com.sun.source.tree.Tree
 import leetcode_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.Solution
+import java.util.Stack
 import kotlin.test.assertEquals
 
 /**
@@ -52,16 +54,53 @@ class Solution {
     }
 }
 
+class Solution2 {
+    fun lowestCommonAncestor(root: TreeNode?, p: TreeNode?, q: TreeNode?): TreeNode? {
+        val stack = Stack<Pair<TreeNode?, Int>>()
+        stack.push(root to 2)
+        var lca: TreeNode? = null
+        var oneNodeFound = false
+        while (stack.isNotEmpty()) {
+            val (parent, state) = stack.pop()
+            if (state == 0) {
+                if (oneNodeFound && lca?.`val` == parent?.`val` && stack.isNotEmpty()) {
+                    lca = stack.peek().first
+                }
+            } else {
+                if (state == 2 && (parent?.`val` == p?.`val` || parent?.`val` == q?.`val`)) {
+                    if (oneNodeFound) {
+                        return lca
+                    }
+                    oneNodeFound = true
+                    lca = parent
+                }
+
+                val child = if (state == 2) {
+                    parent?.left
+                } else {
+                    parent?.right
+                }
+                stack.push(parent to state - 1)
+                if (child != null) {
+                    stack.push(child to 2)
+                }
+            }
+        }
+
+        return lca
+    }
+}
+
 fun main() {
     arrayOf(
-            Triple(TreeNode.create(6, 2, 8, 0, 4, 7, 9, null, null, 3, 5), 2, 8) to 6,
-            Triple(TreeNode.create(6, 2, 8, 0, 4, 7, 9, null, null, 3, 5), 2, 4) to 2,
-            Triple(TreeNode.create(2, 1), 2, 1) to 2,
-            Triple(TreeNode.create(2, 1), 1, 2) to 2,
-            Triple(TreeNode.create(2, 1), 1, 1) to 1,
-            Triple(TreeNode.create(1), 1, 1) to 1,
+//            Triple(TreeNode.create(6, 2, 8, 0, 4, 7, 9, null, null, 3, 5), 2, 8) to 6,
+//            Triple(TreeNode.create(6, 2, 8, 0, 4, 7, 9, null, null, 3, 5), 2, 4) to 2,
+//            Triple(TreeNode.create(2, 1), 2, 1) to 2,
+//            Triple(TreeNode.create(2, 1), 1, 2) to 2,
+            Triple(TreeNode.create(9,-1,-4,10,3,null,null,null,5), 3, 5) to -1
     ).forEach { (input, exp) ->
         val (root, p, q) = input
-        assertEquals(exp, Solution().lowestCommonAncestor(root, TreeNode(p), TreeNode(q))?.`val`)
+//        assertEquals(exp, Solution().lowestCommonAncestor(root, TreeNode(p), TreeNode(q))?.`val`)
+        assertEquals(exp, Solution2().lowestCommonAncestor(root, TreeNode(p), TreeNode(q))?.`val`)
     }
 }

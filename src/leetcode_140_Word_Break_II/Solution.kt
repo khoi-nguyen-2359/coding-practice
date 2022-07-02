10d4bbcd8ebf711370026f9a0cf62a3626af993d
Khoi Nguyen <akhoi90@gmail.com>
2022-07-03 01:36:29 +0700

    leetcode_140_Word_Break_II

--- src/leetcode_140_Word_Break_II/Solution.kt
+++ src/leetcode_140_Word_Break_II/Solution.kt
@@ -1,26 +1,25 @@
 package leetcode_140_Word_Break_II
 
+import java.util.LinkedList
 import java.util.Stack
 
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): List<String> {
         val wordSet = HashSet(wordDict)
-        val stack = Stack<Int>()
-        stack.add(0)
+        val stack = Stack<Node>()
+        stack.add(Node(0))
         val result = mutableListOf<String>()
-        val sentence = Stack<String>()
         while (stack.isNotEmpty()) {
-            val breakIndex = stack.pop()
+            val top = stack.pop()
+            val breakIndex = top.index
             if (breakIndex >= s.length) {
-                // make a sentence from stack
-                val sentence = buildSentence(stack, s)
+                val sentence = buildSentence(top, s)
                 result.add(sentence)
             }
             for (i in breakIndex until s.length) {
                 val word = s.substring(breakIndex, i + 1)
                 if (wordSet.contains(word)) {
-//                    println("add ${i + 1}")
-                    stack.add(i + 1)
+                    stack.add(Node(i + 1, top))
                 }
             }
         }
@@ -28,15 +27,64 @@ class Solution {
         return result
     }
 
-    private fun buildSentence(stack: Stack<Int>, s: String): String = buildString {
-        var lastBreak = 0
-        for (k in stack) {
-//            println(k)
-            append(s.substring(lastBreak, k))
-            append(' ')
-            lastBreak = k
+    private fun buildSentence(node: Node, s: String): String {
+        var curr: Node? = node
+        val words = LinkedList<String>()
+        do {
+            words.add(0, s.substring(curr?.prev?.index ?: 0, curr?.index ?: 0))
+            curr = curr?.prev
+        } while (curr != null)
+        return words.joinToString(" ").trim()
+    }
+
+    class Node(val index: Int, val prev: Node? = null)
+}
+
+class SolutionRecursive {
+    private val wordSet = mutableSetOf<String>()
+    private val memo = mutableMapOf<Int, MutableList<MutableList<String>>>()
+    fun wordBreak(s: String, wordDict: List<String>): List<String> {
+        wordSet.addAll(wordDict)
+        wordBreakRecursive(s, 0)
+        val ans = mutableListOf<String>()
+        val sentences = memo[0] ?: return ans
+        for (sentence in sentences) {
+            println(sentence)
+            val build = buildString {
+                for (i in sentence.size - 1 downTo 0) {
+                    append(sentence[i])
+                    append(' ')
+                }
+            }
+            ans.add(build.trim())
         }
-        removeSuffix(" ")
+        return ans
+    }
+
+    private fun wordBreakRecursive(s: String, start: Int): MutableList<MutableList<String>> {
+        memo[start] ?. let {
+            return it
+        }
+
+        memo[start] = if (start >= s.length) {
+            mutableListOf(mutableListOf())
+        } else {
+            mutableListOf()
+        }
+
+        for (i in start until s.length) {
+            val word = s.substring(start, i + 1)
+            if (wordSet.contains(word)) {
+                val subSentences = wordBreakRecursive(s, i + 1)
+                for (subSent in subSentences) {
+                    val cloneSent = subSent.toMutableList()
+                    cloneSent.add(word)
+                    memo[start]?.add(cloneSent)
+                }
+            }
+        }
+
+        return memo[start] ?: mutableListOf()
     }
 }
 
@@ -46,7 +94,7 @@ fun main() {
             "pineapplepenapple" to listOf("apple", "pen", "applepen", "pine", "pineapple"),
             "catsandog" to listOf("cats", "dog", "sand", "and", "cat"),
     ).forEach { (s, wordDict) ->
-        Solution().wordBreak(s, wordDict).forEach {
+        SolutionRecursive().wordBreak(s, wordDict).forEach {
             println(it)
         }
         println()

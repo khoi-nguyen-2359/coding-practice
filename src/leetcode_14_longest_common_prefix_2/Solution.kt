98c4eb277219a5a6c3df1bcbeb757396e4b4e63c
Khoi Nguyen <akhoi90@gmail.com>
2024-08-29 21:57:31 +0700

    leetcode_14_longest_common_prefix_2

---
+++ src/leetcode_14_longest_common_prefix_2/Solution.kt
@@ -0,0 +1,22 @@
+package leetcode_14_longest_common_prefix_2
+
+// n * min_len
+class Solution {
+    fun longestCommonPrefix(strs: Array<String>): String {
+        var i = 0
+        while (true) {
+            var mismatched = false
+            for (s in strs) {
+                if (strs[0].length <= i || s.length <= i || s[i] != strs[0][i]) {
+                    mismatched = true
+                    break
+                }
+            }
+            if (mismatched) {
+                break
+            }
+            ++i
+        }
+        return strs[0].substring(0, i)
+    }
+}
